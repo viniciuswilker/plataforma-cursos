bfad99494da0adea7bf80dc2fad82f89531d07ef
vinicius wilker <vinicius.zoom360@gmail.com>
2026-01-26 15:20:46 -0300

    pagina de cadastro

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,33 +1,49 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/viniciuswilker/plataforma-cursos/internal/handlers"
 	"github.com/viniciuswilker/plataforma-cursos/internal/middlewares"
 )
 
 func ConfigurarRotas(router *gin.Engine) {
+	// Configuração de templates e arquivos estáticos
+	router.LoadHTMLGlob("templates/*")
+	router.Static("/static", "./static")
 
-	// autenticacao
+	// --- ROTAS WEB (Renderização de HTML) ---
+	web := router.Group("/")
+	{
+		web.GET("/cadastro", func(c *gin.Context) {
+			c.HTML(http.StatusOK, "cadastro.html", nil)
+		})
+		web.GET("/login", func(c *gin.Context) {
+			c.HTML(http.StatusOK, "login.html", nil)
+		})
+	}
+
+	// --- ROTAS DE API (JSON) ---
+
+	// Autenticação
 	auth := router.Group("/auth")
 	{
 		auth.POST("/cadastro", handlers.Cadastrar)
 		auth.POST("/login", handlers.Login)
 	}
 
-	// protegidas
+	// Recursos Protegidos
 	api := router.Group("/api")
-
 	api.Use(middlewares.Autorizar("aluno", "instrutor", "admin"))
 	{
 		api.GET("/cursos", handlers.ListarCursos)
 
-		//  instrutor e admin
+		// Apenas instrutor e admin
 		admin := api.Group("/cursos")
 		admin.Use(middlewares.Autorizar("instrutor", "admin"))
 		{
 			admin.POST("/", handlers.CriarCurso)
 		}
 	}
-
 }
