2b14224987c28015275acc93c531777c3c7d7def
vinicius wilker <vinicius.zoom360@gmail.com>
2026-01-26 14:12:50 -0300

    config banco de dados

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,1 +1,29 @@
 package database
+
+import (
+	"log"
+
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Conectar() {
+	var err error
+
+	DB, err = gorm.Open(sqlite.Open("lms.db"), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Falha ao conectar com o banco de dados: ", err)
+
+	}
+
+	DB.AutoMigrate(
+		&models.Usuario{},
+		&models.Curso{},
+		&models.Aula{},
+		&models.ProgressoAula{},
+	)
+
+}
