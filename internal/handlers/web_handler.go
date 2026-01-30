f942ece4d5698dfea9cb3aa4ec79ed59356df653
vinicius wilker <vinicius.zoom360@gmail.com>
2026-01-30 07:54:43 -0300

    func de verificar se token existe e add nos webhandler

--- internal/handlers/web_handler.go
+++ internal/handlers/web_handler.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/viniciuswilker/plataforma-cursos/internal/repositorios"
+	"github.com/viniciuswilker/plataforma-cursos/internal/services"
 )
 
 // ExibirHome renderiza a página inicial
@@ -14,11 +15,23 @@ func ExibirHome(c *gin.Context) {
 
 // ExibirCadastro renderiza a página de registro
 func ExibirCadastro(c *gin.Context) {
+	tokenString, _ := c.Cookie("token")
+	if services.TokenEhValido(tokenString) {
+		c.Redirect(http.StatusSeeOther, "/feed/")
+		return
+	}
+
 	c.HTML(http.StatusOK, "cadastro.html", nil)
 }
 
 // ExibirLogin renderiza a página de login
 func ExibirLogin(c *gin.Context) {
+	tokenString, _ := c.Cookie("token")
+	if services.TokenEhValido(tokenString) {
+		c.Redirect(http.StatusSeeOther, "/feed/")
+		return
+	}
+
 	c.HTML(http.StatusOK, "login.html", nil)
 }
 
--- internal/services/auth_service.go
+++ internal/services/auth_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -8,6 +9,7 @@ import (
 
 var ChaveSecreta = []byte("sua_chave_secreta_super_segura_123")
 
+// GerarToken cria um token novo
 func GerarToken(usuarioID uint, cargo string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"sub":   usuarioID,
@@ -16,4 +18,24 @@ func GerarToken(usuarioID uint, cargo string) (string, error) {
 	})
 
 	return token.SignedString(ChaveSecreta)
+}
+
+// TokenEhValido verifica se o token foi criado/se o usuario está logado
+func TokenEhValido(tokenString string) bool {
+	if tokenString == "" {
+		return false
+	}
+
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("método de assinatura inesperado")
+		}
+		return ChaveSecreta, nil
+	})
+
+	if err != nil || token == nil {
+		return false
+	}
+
+	return token.Valid
 }
